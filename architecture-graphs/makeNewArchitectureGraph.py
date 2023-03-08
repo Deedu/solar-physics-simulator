ae16f8da692210725437d8c2808c1204af35ec27
David Daugela <daviddaugela@gmail.com>
2023-03-07 22:10:49 -0500

    updated architecture diagram to show things a bit more clearly

--- architecture-graphs/makeNewArchitectureGraph.py
+++ architecture-graphs/makeNewArchitectureGraph.py
@@ -9,17 +9,24 @@ from diagrams.generic.place import Datacenter
 from diagrams.custom import Custom
 
 graph_attr = {
-    # "layout": "dot",
-    # "concentrate": "true",
-    # "compound": "true",
-    # "splines": "spline",
+    "layout": "dot",
+    "concentrate": "true",
+    "compound": "true",
+    # "splines": "spline"
+    # "bgcolor": "orange"
 }
 
-REACT_ICON_PATH = "readmeAssets/react-1-logo-png-transparent.png"
+REACT_ICON_PATH = "assets/react-icon.png"
 GOOGLE_MAPS_ICON_PATH = "assets/google-maps-icon.png"
 OPEN_METEO_ICON_PATH = "assets/open-meteo-icon.png"
 
 with Diagram("Solar Physics Simulator", show=False, graph_attr=graph_attr):
+
+    with Cluster("Client Side"):
+        with Cluster("Simulation App\r(app.greatestdemoever.com)"):
+            react_app = [Custom("React App", REACT_ICON_PATH)]
+
+
     with Cluster("GCP Cloud Backend"):
         # firestoreIcon = Firestore("Database")
 
@@ -36,26 +43,23 @@ with Diagram("Solar Physics Simulator", show=False, graph_attr=graph_attr):
 
         with Cluster("Simulation Processing"):
             simulation_processing = [GKE("simulation"), GKE("simulation"), GKE("simulation")]
-            flow >> simulation_processing[1]
-            simulation_processing[1] >> bigqueryIcon
+            flow >> simulation_processing
+            simulation_processing >> bigqueryIcon
 
         request_handling >> flow
+        react_app >> request_handling
+        request_handling >> react_app
 
-    with Cluster("Client Side"):
-        with Cluster("Simulation App\r(app.greatestdemoever.com)"):
-            react_app = [Hosting("React App")]
-            react_app[0] >> request_handling
-            request_handling >> react_app[0]
 
-    with Cluster("Weather Data"):
+
+    with Cluster("External APIs"):
         weather_cluster = [Custom("OpenMeteo API", OPEN_METEO_ICON_PATH)]
         # weather_cluster >> simulation_processing[0]
         weather_cluster >> simulation_processing[1]
-
-    with Cluster("Google Maps API"):
         google_maps_cluster = [Custom("Google Maps API", GOOGLE_MAPS_ICON_PATH)]
         # google_maps_cluster >> simulation_processing[0]
         google_maps_cluster >> simulation_processing[1]
+
 
 # with Cluster("Notifications For Users"):
 #     twillio = Custom("Twillio", TWILIO_ICON_PATH)
